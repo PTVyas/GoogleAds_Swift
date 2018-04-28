ca6aa090c9e66bd78eefa534003789fdaa8731d1
PTVyas <vyasbhagat@gmail.com>
2018-04-28 12:24:53 +0530

    Initial Commit

---
+++ GoogleAds_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleAds_Swift
+//
+//  Created by WOS_MacMini_1 on 28/04/18.
+//  Copyright © 2018 White Orange Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
